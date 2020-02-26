e760fa4d46c7ad231895e746041366f7b4efcb2b
mathemacode <43391344+mathemacode@users.noreply.github.com>
2020-02-25 21:00:48 -0500

    Participants file needs indexes

--- R/combine_export_participants.R
+++ R/combine_export_participants.R
@@ -51,7 +51,7 @@ for (filename in list.files(path = path)) {
   
 }
 
-write.csv(AllParticipants, paste0(exportpath,'/','All_Participants.csv'), row.names = FALSE)
+write.csv(AllParticipants, paste0(exportpath,'/','All_Participants.csv'), row.names = TRUE)
 
 # Clean up R Studio
 remove(path, exportpath, filename, year, df)
