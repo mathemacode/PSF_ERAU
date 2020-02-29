5fdba828ab40a2e3ff51c7e28f2a9a2cbeeccae9
mathemacode <43391344+mathemacode@users.noreply.github.com>
2020-02-28 20:31:15 -0500

    Rename & Add extra verification to PK testing script

--- R/df_PK_test.R
+++ R/df_PK_test.R
@@ -1,6 +1,6 @@
 # Is the dataframe clean and ready for PK/FK in database?
 
-df <- AllCases
+df <- data.frame(AllCasesUQ)
 PK <- 'InternalCaseID'
 # PK <- 'IdentificationID'
 
@@ -8,8 +8,20 @@ PK <- 'InternalCaseID'
 print(paste0("Unique PKs: ", length(unique(df[, c(PK)]))))
 print(paste0("Number PKs: ", length(df[, c(PK)])))
 
+
+# If the number of unique PKs is the same length as the column
+# (all rows in column have a unique value in the PK column)
+
+if ((length(df[, c(PK)]) - length(unique(df[, c(PK)]))) == 0) {
+  print("SUCCESS!  PK IS VERIFIED")
+} else {
+  print("WILL NOT WORK AS PK")
+}
+
 # Find duplicated PKs
 duplicates <- df[df[, c(PK)] %in% df[, c(PK)][duplicated(df[, c(PK)])],]
 
+# Remove duplicates from df
+# no_duplicates <- df[ !(df[, c(PK)] %in% df[, c(PK)][duplicated(df[, c(PK)])]),]
 
 remove(df, PK, duplicates)
