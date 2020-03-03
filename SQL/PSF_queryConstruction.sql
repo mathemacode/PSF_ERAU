18880cbc3a8312d2f04e8a918186fc4a535d81e4
mathemacode <43391344+mathemacode@users.noreply.github.com>
2020-03-02 20:02:32 -0500

    Build more useful queries

--- SQL/PSF_queryConstruction.sql
+++ SQL/PSF_queryConstruction.sql
@@ -1,15 +1,35 @@
 -- Construct useful SQL queries
 
+-- Find number of cases particular IdentificationID appears in
+SELECT IdentificationID, COUNT(InternalCaseID) AS NumCases
+FROM PARTICIPANTS
+GROUP BY IdentificationID
+ORDER BY NumCases DESC;
+
 -- Find number of cases per CHILD
 SELECT IdentificationID, COUNT(InternalCaseID) AS NumCases_Child
 FROM PARTICIPANTS
-WHERE ServiceRole = 'Child'
+WHERE ServiceRole IN('Child', 'Child Receiving Services')
 GROUP BY IdentificationID
 ORDER BY NumCases_Child DESC;
 
 -- Find number of cases per ADULT
 SELECT IdentificationID, COUNT(InternalCaseID) AS NumCases_Adult
 FROM PARTICIPANTS
-WHERE ServiceRole NOT IN('Child', 'Child Not Receiving Service')
+WHERE ServiceRole NOT IN('Child', 'Child Receiving Services', 
+						 'Child Not Receiving Services', 'Child Not Receiving Service')
 GROUP BY IdentificationID
 ORDER BY NumCases_Adult DESC;
+
+-- Check specific record
+SELECT InternalCaseID, IdentificationID, ServiceRole
+FROM PARTICIPANTS
+WHERE IdentificationID = 10101661791;
+
+
+-- Number of cases per each IdentificationID per each year
+SELECT IdentificationID, COUNT(InternalCaseID) AS NumCases, RecordYear
+FROM PARTICIPANTS
+GROUP BY RecordYear, IdentificationID
+ORDER BY NumCases DESC, IdentificationID, RecordYear;
+
