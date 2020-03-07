6e0f2061c6582a690042d228c8930ab68acad748
mathemacode <43391344+mathemacode@users.noreply.github.com>
2020-03-06 21:00:43 -0500

    Use DISTINCT() to find unique cases each ID is in

--- SQL/PSF_queryConstruction.sql
+++ SQL/PSF_queryConstruction.sql
@@ -1,28 +1,28 @@
 -- Construct useful SQL queries
 
 -- Find number of cases particular IdentificationID appears in
-SELECT IdentificationID, COUNT(InternalCaseID) AS NumCases
+SELECT IdentificationID, COUNT(DISTINCT(InternalCaseID)) AS NumCases
 FROM PARTICIPANTS
 GROUP BY IdentificationID
 ORDER BY NumCases DESC;
 
 -- Find number of cases particular IdentificationID appears in
 -- With added info
-SELECT IdentificationID, Gender, Ethnicity, COUNT(InternalCaseID) AS NumCases
+SELECT IdentificationID, Gender, Ethnicity, COUNT(DISTINCT(InternalCaseID)) AS NumCases
 FROM PARTICIPANTS
 WHERE Ethnicity IS NOT NULL
 GROUP BY IdentificationID, Gender, Ethnicity
 ORDER BY NumCases DESC;
 
 -- Find number of cases per CHILD
-SELECT IdentificationID, COUNT(InternalCaseID) AS NumCases_Child
+SELECT IdentificationID, COUNT(DISTINCT(InternalCaseID)) AS NumCases_Child
 FROM PARTICIPANTS
 WHERE ServiceRole IN('Child', 'Child Receiving Services')
 GROUP BY IdentificationID
 ORDER BY NumCases_Child DESC;
 
 -- Find number of cases per ADULT
-SELECT IdentificationID, COUNT(InternalCaseID) AS NumCases_Adult
+SELECT IdentificationID, COUNT(DISTINCT(InternalCaseID)) AS NumCases_Adult
 FROM PARTICIPANTS
 WHERE ServiceRole NOT IN('Child', 'Child Receiving Services', 
 						 'Child Not Receiving Services', 'Child Not Receiving Service')
@@ -36,7 +36,7 @@ WHERE IdentificationID = 10101661791;
 
 
 -- Number of cases per each IdentificationID per each year
-SELECT IdentificationID, COUNT(InternalCaseID) AS NumCases, RecordYear
+SELECT IdentificationID, COUNT(DISTINCT(InternalCaseID)) AS NumCases, RecordYear
 FROM PARTICIPANTS
 GROUP BY RecordYear, IdentificationID
 ORDER BY NumCases DESC, IdentificationID, RecordYear;
