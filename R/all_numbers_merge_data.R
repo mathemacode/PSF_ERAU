514f85663f62f99bd373ed1e05814905c109e919
mathemacode <43391344+mathemacode@users.noreply.github.com>
2020-10-19 15:59:15 -0700

    Completely overhaul ML dataframe creation process

--- R/all_numbers_merge_data.R
+++ R/all_numbers_merge_data.R
@@ -1,15 +1,23 @@
 #
-# This program was edited from the original for new PSF_ERAU repo.
+# This program was edited from the original repo for this
+# new PSF_ERAU repo.  I've made the loop much more condensed,
+# and now it is using data from csv files.
 #
-# This program creates the dataframe "all_numbers"
+# This program creates the dataframe "ML_frame"
 # which can be used for machine learning.  It is designed
 # to have as much information as possible, WITHOUT ANY NA'S
 # in order to make the machine learning programs as 
 # accurate as possible.
 #
-# Columns of all_numbers are filled with the appropriate data
-# per each case, zip code, or other factor that aligns with info
+# Columns of ML_frame are filled with the appropriate data
+# per each case, zip code, or other factor that aligns with info.
+# See ~line 105 for the exact columns included.
+#
+# This program also does some feature engineering to find ages
+# of participants, case duration, number of removals, etc.
+#
 
+library(pastecs)
 library(dplyr)
 library(readr)
 
@@ -19,10 +27,11 @@ library(readr)
 All_Cases <- read_csv("GitHub/PSF_ERAU/data/csv/All_Cases.csv", 
                       col_types = cols(
                         InternalCaseID = col_double(),
-                        CaseClosedDate = col_date(format = "%Y-%m-%d %H:%M:%S"),
-                        CaseOpenDate = col_date(format = "%Y-%m-%d %H:%M:%S"),
+                        CaseClosedDate = col_datetime(format = "%Y-%m-%d %H:%M:%S"),
+                        CaseOpenDate = col_datetime(format = "%Y-%m-%d %H:%M:%S"),
                         CaseType = col_character(), InternalCaseID = col_integer(),
-                        Region = col_character(), Zip = col_integer()), 
+                        Region = col_character(), 
+                        Zip = col_integer()), 
                       na = "NA")
 
 
@@ -94,249 +103,109 @@ All_Removals <- read_csv("GitHub/PSF_ERAU/data/csv/All_Removals.csv",
                         na = "NA")
 
 
+ML_frame = data.frame(id = double(),
+                      zip = integer(),
+                      zip_count = integer(),
+                      number_removals = double(),
+                      number_placements = double(),
+                      number_participants = double(),
+                      case_duration_yrs = double(),
+                      number_caregivers = integer(),
+                      age_child = double(),
+                      avg_age_caregiver = double(),
+                      stringsAsFactors=FALSE) 
 
-cases_relevant <- filter(cases, cases$`InternalCaseID` %in% case_rem_place$`InternalCaseID`)
+
+cases <- filter(All_Cases, All_Cases$`InternalCaseID` %in% All_Placements$`InternalCaseID`)
+cases <- filter(cases, !is.na(cases$Zip))
+participants <- filter(All_Participants, All_Participants$`InternalCaseID` %in% cases$`InternalCaseID`)
+removals <- filter(All_Removals, All_Removals$`InternalCaseID` %in% cases$`InternalCaseID`)
+placements <- filter(All_Placements, All_Placements$`InternalCaseID` %in% cases$`InternalCaseID`)
+
 
 # ===================================================================================
 
-# Put caseID into all_numbers
+# Load all participant ID's into empty ML_frame
+# for testing purposes isolate specific case, use --> ML_frame[1,"id"] <- 10006247892
+
+children <- filter(participants, participants$ServiceRole == "Child")
+
 i <- 1
-for (ID in all_numbers$`Identification ID`){
+for (id in unique(children$IdentificationID)){
   
-  spot <- which(case_rem_place$`Identification ID` == ID)
-  all_numbers[i,1] <- case_rem_place[spot[1], 1]
+  ML_frame[i,] <- c(id, NA, NA, NA, NA, NA, NA, NA, NA, NA)
   
   i = i+1
-  
 }
 
 # ===================================================================================
 
-# Put zip code into all_numbers (some do not have zips)
+# Add all details; feature engineering per each row included 
+
 i <- 1
-for (caseID in all_numbers$`InternalCaseID`){
+for (id in ML_frame$id){
   
-  spot <- which(cases_relevant$`InternalCaseID` == caseID)
-  all_numbers[i,4] <- as.double(cases_relevant[spot[1], 5])
+  # first zip code in system for individual's case, use to find CaseID in cases df
+  case_id_of_participant <- filter(participants, participants$IdentificationID == id)$InternalCaseID[1]
+  zip_code <- filter(cases, cases$InternalCaseID == case_id_of_participant)$Zip
+  ML_frame[i,"zip"] <- zip_code
+  
+  # Zip count
+  zip_count <- nrow(filter(cases, cases$Zip == zip_code))
+  ML_frame[i,"zip_count"] <- zip_count
+  
+  # Number of removals
+  num_removals <- length(unique((filter(removals, removals$IdentificationID == id)$RemovalDate)))
+  ML_frame[i,"number_removals"] <- num_removals
+  
+  # Number of placements
+  num_placements <- length(unique((filter(placements, placements$IdentificationID == id)$PlacementBeginDate)))
+  ML_frame[i,"number_placements"] <- num_placements
+  
+  # Number of participants
+  one_case <- filter(participants, participants$InternalCaseID == case_id_of_participant)
+  num_participants <- (length(unique(one_case$IdentificationID)) - 1)  # Note that other children may be included
+  ML_frame[i,"number_participants"] <- num_participants
+  
+  # Case Duration -- if this is "NA", it's probably still an open case
+  case_details <- filter(cases, cases$InternalCaseID == case_id_of_participant)
+  duration <- as.double(case_details$CaseClosedDate - case_details$CaseOpenDate) / 365
+  ML_frame[i,"case_duration_yrs"] <- round(duration,1)
+  
+  # Number of Caregivers
+  case_part <- filter(participants, participants$InternalCaseID == case_id_of_participant)
+  caregivers <- filter(case_part, case_part$ServiceRole == "Primary Caregiver" | case_part$ServiceRole == "Secondary Caregiver" |
+                         case_part$ServiceRole == "Parent In The Home" | case_part$ServiceRole == "Parent Not In The Home")
+  num_caregivers <- length(unique(caregivers$IdentificationID))
+  ML_frame[i,"number_caregivers"] <- num_caregivers
+  
+  # Average age of caregiver
+  avg_age_caregiver <- (as.double(mean(as.Date(ISOdate(caregivers$RecordYear, 1, 1)) - caregivers$MonthOfBirth))/365)
+  ML_frame[i,"avg_age_caregiver"] <- round(avg_age_caregiver, 1)
+  
+  # Age of child
+  month_of_birth_child <- filter(participants, participants$IdentificationID == id)$MonthOfBirth[1]
+  date_of_case_child <- filter(cases, cases$InternalCaseID == case_id_of_participant)$CaseOpenDate
+  age_of_child <- as.double(as.Date(date_of_case_child)  - as.Date(month_of_birth_child)) / 365
+  ML_frame[i,"age_child"] <- round(age_of_child, 1)
   
   i = i+1
-  
 }
 
-# Remove data without zip code
-all_numbers <- filter(all_numbers, !is.na(all_numbers$Zip))
+# Remove any records missing removals or placements
+ML_frame <- filter(ML_frame, ML_frame$number_removals > 0)
+ML_frame <- filter(ML_frame, ML_frame$number_placements > 0)
+ML_frame <- filter(ML_frame, ML_frame$age_child > 0)
+ML_frame <- filter(ML_frame, ML_frame$avg_age_caregiver > 0)
 
-# ===================================================================================
+# Incomplete rows for debugging:
+incomplete <- ML_frame[!complete.cases(ML_frame),]
 
-# Is zip code in top 7 highest cases/popdens area or out?
-# 1 = in, 0 = out
-i <- 1
-zips <- c(32331, 32359, 32060, 32680, 32626, 32348, 32055)
+# Only include complete rows with all data for ML models
+ML_frame <- ML_frame[complete.cases(ML_frame),]
 
-for (zipcode in all_numbers$`Zip`){
-  
-  spot <- which(cases_relevant$`zip5` == zipcode)
-  
-  if (zipcode %in% zips) {
-    all_numbers[i,5] <- 1
-    
-  }
-  else {
-    all_numbers[i,5] <- 0
-  }
-  
- 
-  i = i+1
-  
-}
-
-# ===================================================================================
-
-# Find number of participants per child's case
-i <- 1
-
-uniqueIDs <- unique(all_numbers$`Identification ID`)
-
-for (ID in uniqueIDs) {
-  
-  spot <- which(all_numbers$`Identification ID` == ID)
-  caseID <- as.double(all_numbers[spot[1], 1])
-  
-  one_case <- filter(ALL_PARTICIPANTS, ALL_PARTICIPANTS$`PseudoCaseID` == caseID)
-  number_of_participants <- length(unique(one_case$`Identification ID`))
-  
-  for (place in spot) {
-    all_numbers[place,7] <- number_of_participants
-  }
-  
-  i = i+1
-  
-}
-
-# ===================================================================================
-
-# Add Zip Count (number of cases in Zip) to all_numbers
-i <- 1
-for (zip in all_numbers$Zip) {
-  
-  all_numbers[i,9] <- nrow(filter(cases, cases$zip5 == zip))
-  
-  i = i+1
-  
-}
-
-# Convert these character strings into doubles before moving on
-all_numbers$ZipCount <- as.double(all_numbers$ZipCount)
-
-# ===================================================================================
-
-# Add Zip Density (Pop Density of Zip) to all_numbers
-i <- 1
-for (zip in all_numbers$Zip) 
-  {
-  spot <- which(PopDensity$`Zip/ZCTA` == zip)
-  all_numbers[i,10] <- (PopDensity[spot[1], 4])
-  
-  i = i+1
-  }
-
-# ZipDens is a number --> make it a double
-all_numbers$ZipDens <- as.double(all_numbers$ZipDens)
-
-# Remove data that does not have ZipDens (missing Zip or density in calculation)
-all_numbers <- filter(all_numbers, !is.na(all_numbers$ZipDens))
-
-# ===================================================================================
-
-# Case Duration
-i <- 1
-for (caseID in all_numbers$`InternalCaseID`){
-  
-  spot <- which(cases$`InternalCaseID` == caseID)
-  
-  all_numbers[i,11] <- (cases[spot[1], 4] - cases[spot[1], 3])
-  
-  i = i+1
-  
-}
-
-all_numbers <- filter(all_numbers, !is.na(all_numbers$CaseDuration))
-
-# ===================================================================================
-
-# Age of child
-copy <- filter(ALL_PARTICIPANTS, ALL_PARTICIPANTS$MonthOfBirth != "/")
-copy$Year <- gsub("^", "01/01/", copy$Year)
-copy$Year <- as.Date.character(copy$Year, format="%d/%m/%Y")
-
-# Change format of birthdates to MM/DD/YYYY instead of MM/YYY
-copy$MonthOfBirth <- gsub("/", "/01/", copy$MonthOfBirth)
-copy$MonthOfBirth <- as.Date.character(copy$MonthOfBirth, format="%d/%m/%Y")
-
-# Subtract case date and birth date to find age of child during case
-i <- 1
-for (ID in all_numbers$`Identification ID`){
-  
-  spot <- which(copy$`Identification ID` == ID)
-  all_numbers[i,12] <- (copy[spot[1], 36] - copy[spot[1], 6])
-  
-  i = i+1
-  
-}
-
-# Divide by 365 to convert age in days to age in years
-all_numbers$Age <- all_numbers$Age / 365
-
-# ===================================================================================
-
-# Number of caregivers in case and the average age of them
-# Start by making columns, fill with 1's before overwriting
-
-all_numbers <- mutate(all_numbers, "NumCaregivers" = 1)
-all_numbers <- mutate(all_numbers, "CareAge" = 1)
-
-i <- 1
-uniqueIDs <- unique(all_numbers$`Identification ID`)
-
-for (ID in uniqueIDs) {
-  
-  spot <- which(all_numbers$`Identification ID` == ID)
-  caseID <- as.double(all_numbers[spot[1], 1])
-  
-  one_case <- filter(copy, copy$`PseudoCaseID` == caseID)
-  caregivers <- filter(one_case, one_case$`Service Role` == "Primary Caregiver" | one_case$`Service Role` == "Secondary Caregiver")
-  
-  number_of_caregivers <- length(unique(caregivers$`Identification ID`))
-  
-  # Find ages of each caregiver, sum them / number_of_caregivers = average age of caregiver in case
-  # Add this number to the same record in additional column in all_numbers
-  
-  average <- mean(caregivers$`Year` - caregivers$`MonthOfBirth`)
-  
-  for (careID in caregivers$`Identification ID`){
-    
-    all_numbers[i,14] <- average / 365
-    
-  }
-  
-  
-  for (careID in caregivers$`Identification ID`) {
-    all_numbers[i,13] <- number_of_caregivers
-  }
-  
-  i = i+1
-  
-}
-
-# Remove any records that have no caregivers on record
-# This means that number of caregivers = 0 and age of caregivers = 0 or INF (div 0)
-all_numbers <- filter(all_numbers, all_numbers$NumCaregivers != 0)
-all_numbers <- filter(all_numbers, !is.na(all_numbers$CareAge))
-
-
-# Merge in dataset with crime information
-# ===================================================================================
-
-all_numbers <- mutate(all_numbers, "Per-Capita-Income" = 1)
-all_numbers <- mutate(all_numbers, "Median-Household-Income"  = 1)
-all_numbers <- mutate(all_numbers, "VC-(1-to-100)"  = 1)
-all_numbers <- mutate(all_numbers, "PC (1 to 100)"  = 1)
-all_numbers <- mutate(all_numbers, "Avg-Home-$$$"  = 1)
-all_numbers <- mutate(all_numbers, "Infant-Mortailiy-Rate"  = 1)
-all_numbers <- mutate(all_numbers, "Percent-of-Population-Under-18"  = 1)
-all_numbers <- mutate(all_numbers, "Low-Birth-Weight"  = 1)
-all_numbers <- mutate(all_numbers, "Juvenile-Dilquency-(Arrests-per-1000-Juvineles)"  = 1)
-
-
-i <- 1
-for (zipcode in all_numbers$`Zip`){
-  
-  spot <- which(crime$`ZIP` == zipcode)
-  
-  all_numbers[i,15] <- (crime[spot[1], 4])
-  all_numbers[i,16] <- (crime[spot[1], 5])
-  all_numbers[i,17] <- (crime[spot[1], 6])
-  all_numbers[i,18] <- (crime[spot[1], 7])
-  all_numbers[i,19] <- (crime[spot[1], 8])
-  all_numbers[i,20] <- (crime[spot[1], 9])
-  all_numbers[i,21] <- (crime[spot[1], 10])
-  all_numbers[i,22] <- (crime[spot[1], 11])
-  all_numbers[i,23] <- (crime[spot[1], 12])
-  
-  i = i+1
-  
-}
-
-# ===================================================================================
-
-# Remove records that did not have per capita income OR case start/end dates (end-start would be 0)
-all_numbers <- filter(all_numbers, !is.na(all_numbers$"Per-Capita Income"))
-all_numbers <- filter(all_numbers, !is.na(all_numbers$CaseDuration))
-
-# ===================================================================================
-
-# Clean up R Studio
-remove(uniqueIDs, zips, zipcode, spot, i, ID, caseID, number_of_participants, one_case, place, zip, caregivers, number_of_caregivers)
+# Summary of ML_frame
+summary_df <- stat.desc(ML_frame)
 
 # Write to file
-write.csv(all_numbers, "all_numbers.csv")
+# write.csv(ML_frame, "ML_frame.csv")
