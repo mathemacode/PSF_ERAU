0c59606ae2d7fb0c7003ced5c072af00e2939fcf
mathemacode <43391344+mathemacode@users.noreply.github.com>
2021-03-13 16:52:33 -0500

    More useful calculations for paper

--- R/plotting.R
+++ R/plotting.R
@@ -4,14 +4,22 @@ length(unique(All_Cases$InternalCaseID))
 # Number of unique participants
 length(unique(All_Participants$IdentificationID))
 
-# Number of unique children
+# After filtering, number of unique participants
+length(unique(participants$IdentificationID))
+
+# Number of unique children (before filtering) (after filtering = nrow(ML_frame))
 children <- filter(All_Participants, All_Participants$ServiceRole == 'Child')
 length(unique(children$IdentificationID))
-
 
 # Male vs. Female Children Distribution
 nrow(filter(All_Participants, All_Participants$ServiceRole == 'Child' & All_Participants$Gender == 'Male'))
 nrow(filter(All_Participants, All_Participants$ServiceRole == 'Child' & All_Participants$Gender == 'Female'))
+
+# Male vs. Female Caregiver Distribution
+male_careg <- nrow(filter(All_Participants, All_Participants$ServiceRole == 'Primary Caregiver' & All_Participants$Gender == 'Male'))
+female_careg <- nrow(filter(All_Participants, All_Participants$ServiceRole == 'Primary Caregiver' & All_Participants$Gender == 'Female'))
+
+percent_female_careg <- (female_careg / (female_careg + male_careg))*100
 
 # Record Year Distribution
 barplot(table(All_Participants$RecordYear), 
@@ -24,6 +32,7 @@ barplot(table(All_Participants$RecordYear),
 
 mean(table(All_Participants$RecordYear))
 
-# Ethnicities
-sort(table(All_Participants$Ethnicity), decreasing=TRUE)[1:10]
+# Ethnicities Percentages
+ethn_perc <- sort((sort(table(All_Participants$Ethnicity), decreasing=TRUE)[1:10] / 
+                      sum(table(All_Participants$Ethnicity))) * 100, decreasing=TRUE)
 
